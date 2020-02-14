7c54dab52c6da9585f8518e3d0a730fe2c285a66
JubeatCan <zzlxxxsc@gmail.com>
2020-02-14 11:08:30 -0600

    sort driver function

--- Main/DatabaseTable/source/Sorting.cc
+++ Main/DatabaseTable/source/Sorting.cc
@@ -12,14 +12,16 @@
 
 using namespace std;
 
-void mergeIntoFile (MyDB_TableReaderWriter &sortIntoMe, vector <MyDB_RecordIteratorAltPtr> &mergeUs, function <bool ()> comparator, MyDB_RecordPtr lhs, MyDB_RecordPtr rhs) {}
+void mergeIntoFile (MyDB_TableReaderWriter &sortIntoMe, vector <MyDB_RecordIteratorAltPtr> &mergeUs, function <bool ()> comparator, MyDB_RecordPtr lhs, MyDB_RecordPtr rhs) {
+
+}
 
 vector <MyDB_PageReaderWriter> mergeIntoList (MyDB_BufferManagerPtr parent, MyDB_RecordIteratorAltPtr leftIter, MyDB_RecordIteratorAltPtr rightIter, function <bool ()> comparator,
 	MyDB_RecordPtr lhs, MyDB_RecordPtr rhs) {return vector <MyDB_PageReaderWriter> (); }
 	
 void sort (int runSize, MyDB_TableReaderWriter &sortMe, MyDB_TableReaderWriter &sortIntoMe, function <bool ()> comparator, MyDB_RecordPtr lhs, MyDB_RecordPtr rhs) {
     MyDB_BufferManagerPtr BufferPtr = sortMe.getBufferMgr();
-    vector<MyDB_RecordIteratorAlt> mergeUs;
+    vector<MyDB_RecordIteratorAltPtr> mergeUs;
 
     for (int i = 0; i < sortMe.getNumPages();) {
         vector< vector<MyDB_PageReaderWriter>> mergeHelper;
@@ -33,11 +35,19 @@ void sort (int runSize, MyDB_TableReaderWriter &sortMe, MyDB_TableReaderWriter &
             mergeHelper.push_back(tempVector);
         }
 
-        int endThisRun = j;
         while (mergeHelper.size() != 1) {
-            mergeHelper.insert(mergeIntoList(BufferPtr, mergeHelper[0]));
+            mergeHelper.push_back(mergeIntoList(BufferPtr, getIteratorAlt(mergeHelper[0]), getIteratorAlt(mergeHelper[1]), comparator, lhs, rhs));
+            mergeHelper.erase(mergeHelper.begin());
+            mergeHelper.erase(mergeHelper.begin());
         }
+
+        mergeUs.push_back(getIteratorAlt(mergeHelper[0]));
+
+        //update i
+        i = j;
     }
+
+    mergeIntoFile(sortIntoMe, mergeUs, comparator, lhs, rhs);
 }
 
 #endif
